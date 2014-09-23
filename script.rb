884b95a60322e12021cb25cd744825453f3b3df3
André Veríssimo <afsverissimo@gmail.com>
2014-09-23 12:26:25 +0100

    adds verbose and example file

--- script.rb
+++ script.rb
@@ -1,8 +1,15 @@
 require "./blast"
 
+# example for dbs named 'c1', 'c2' and 'c1_l1_2'
 dbs = ["c1","c2", "c1_l1_2"]
+# example for queries in folders: 'kegg_queries' and 'ncbi_queries'
 queries = ["kegg_queries","ncbi_queries"]
+# create Blast object
 b = Blast.new( dbs )
-#b.blastn_folders( queries )
+#
+# example of performing just one blast query
 #b.blastn('kegg_queries/quer1cetin.query',"c2", "out/test.out")
+#
+# blast folders
+b.blastn_folders( queries )
 b.gen_report_from_output
