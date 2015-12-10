24156bca080813eb8804f1298277d48b7244a03c
André Veríssimo <afsverissimo@gmail.com>
2015-12-10 17:20:09 +0100

    adds cleaning method to rake that cleans everything

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,14 @@
 require 'rake/testtask'
+require 'rake/clean'
+require 'byebug'
 
 Rake::TestTask.new do |t|
-  t.libs << "test"
+  t.libs << 'test'
   t.test_files = FileList['test/tc*.rb']
   t.verbose = true
+end
+
+Rake::FileList.new('output/*') do |fl|
+  CLOBBER << fl
+  CLEAN << fl
 end
