748e36ad0a47f86c4396085b70475b6ad7650bf6
André Veríssimo <afsverissimo@gmail.com>
2015-12-14 19:52:12 +0100

    working set for ORF class

--- orf.rb
+++ orf.rb
@@ -1,3 +1,4 @@
+require 'logger'
 require 'bio'
 #
 #
@@ -8,38 +9,164 @@ class ORF
                       stop:  %w(tag taa tga),
                       reverse: true,
                       direct: true,
-                      min: 6 }
-  #
+                      min: 6,
+                      default_to_seq: false,
+                      debug: false }
 
-  def self.find_idx(seq, idx, option_name, cmp_sign, fallback_value, options)
-    option_name = option_name.to_sym
-    # if start option does not exist, then should
-    #  treat start of sequence as the start
-    if options[option_name] && !options[option_name].empty?
-      # iterate over all start codons to see which
-      #  is best
-      options[option_name].each do |codon|
-        # must add +1 to index, as splice method
-        #  takes position [1, end] instead of [0, end]
-        #  as ruby string methods
-        temp_idx = seq.index(codon)
-        next if temp_idx.nil?
-        temp_idx += 1 # due to difference in indexes
-        # only take new idx if it is really better
-        if cmp_sign == '<'
-          idx = temp_idx if temp_idx < idx
-        elsif cmp_sign == '>'
-          idx = temp_idx if temp_idx > idx
-        end
-      end
-    else
-      # default position if no start option is the beggining
-      idx = fallback_value
+  attr_reader :logger, :options, :seq, :sequence
+  attr_writer :options
+
+  def longest
+    find if @orf.nil?
+    res = { frame1: nil, frame2: nil, frame3: nil }
+    @orf.each do |key, val|
+      res[key] = get_range(val[:longest])
     end
-    idx
+    res
   end
 
-  def self.lookup_codons_idx(seq, option_name, options)
+  def aa
+    find if @orf.nil?
+    res = longest
+    res.each do |key, val|
+      res[key] = val.translate
+    end
+    res
+  end
+
+  def nt
+    find if @orf.nil?
+    longest
+  end
+
+  def initialize(sequence, options = {})
+    sequence = Bio::Sequence::NA.new(sequence) if sequence.class == String
+    @sequence = sequence
+    @seq = @sequence.to_s
+    #
+    self.options = DEFAULT_OPTIONS.merge(options.nil? ? {} : options)
+    @logger      = Logger.new(STDOUT)
+    if options[:debug]
+      logger.level = Logger::INFO
+    else
+      logger.level = Logger::UNKNOWN
+    end
+  end
+  #
+  # For a given sequence, find longest ORF
+  #
+
+  def self.find(sequence, options = {})
+    # merge options with default
+    orf = ORF.new(sequence, options)
+    @result = orf.find
+    #
+  end
+
+  #
+  #
+  #
+  def find
+    return sequence if sequence.nil? || sequence.size == 0
+    #
+    start_idx = lookup_codons_idx(:start)
+    stop_idx  = lookup_codons_idx(:stop)
+    res = get_longest(start_idx, stop_idx, seq.size, [0, 1, 2])
+    logger.info "start codons idx: #{start_idx}"
+    logger.info "stop codons idx: #{stop_idx}"
+    logger.info res
+    @orf = { frame1: {}, frame2: {}, frame3: {} }
+    # iterate over each frame and range to return the
+    #  longest above the minimum sequence length
+    # these are the preferences:
+    #  1: range that has start and stop codons
+    #  2: range that only has start/stop
+    #  3: full sequence
+    res.each_with_index do |frame, index|
+      frame_val = []
+      frame_fal = []
+      frame.each do |range|
+        if range[:fallback]
+          frame_fal << range
+        else
+          frame_val << range
+        end
+      end
+      hash_name = "frame#{index + 1}".to_sym
+      @orf[hash_name][:orfs] = (frame_val.empty? ? frame_fal : frame_val)
+      longest = { len: nil, range: nil }
+      @orf[hash_name][:orfs].each do |range|
+        len = range[:stop] - range[:stop] + 1
+        if longest[:range].nil? || len > longest[:len]
+          longest[:len]   = len
+          longest[:range] = range
+        end
+      end
+      @orf[hash_name][:longest] = longest[:range]
+    end
+    #
+    if options[:debug]
+      @orf.each do |key, frame|
+        frame[:orfs].each do |range|
+          print_range(key, range)
+        end
+      end
+    end
+    #
+    @orf
+  end
+
+  private
+
+  def get_range(arg1, arg2 = nil)
+    if arg2.nil?
+      start = arg1[:start]
+      stop = arg1[:stop]
+    else
+      start = arg1
+      stop = arg2
+    end
+    Bio::Sequence::NA.new(get_range_str(start, stop))
+  end
+
+  def get_range_str(start, stop)
+    seq[start..stop]
+  end
+
+  #
+  # auxiliary method that prints range
+  #
+  def print_range(key, range)
+    # simple proc to add spaces, works as auxiliary
+    #  method to print range
+    add_spaces = proc do |str|
+      str.gsub(/(.{1})/, '\1 ').strip
+    end
+
+    orf = add_spaces.call(get_range_str(range[:start], range[:stop]))
+    pre = if range[:start] == 0
+            ''
+          else
+            add_spaces.call(get_range_str(0, range[:start] - 1))
+          end
+    suf = if range[:end] == seq.size - 1
+            ''
+          else
+            add_spaces.call(get_range_str(range[:stop] + 1, seq.size - 1))
+          end
+    #
+    sep = '|'
+    str = "#{key}: #{pre}#{sep}#{orf}#{sep}#{suf}"
+    str += ' : ' \
+      "size=#{seq[range[:start]..range[:stop]].size}"
+    str += ' (fallback)' if range[:fallback]
+    logger.info str
+  end
+
+  #
+  # Find all indexes for valid codons (either for :start
+  #  or :stop)
+  def lookup_codons_idx(option_name)
     idxs = []
     option_name = option_name.to_sym
     # if start option does not exist, then should
@@ -67,33 +194,11 @@ class ORF
   end
 
   #
-  def self.find2(sequence, options)
-    sequence = Bio::Sequence::NA.new(sequence) if sequence.class == String
-    return sequence if sequence.nil? || sequence.size == 0
-    # string should be more efficient
-    seq = sequence.to_s
-    #
-    options[:seq] = seq
-    #
-    start_idx = lookup_codons_idx(seq, :start, options)
-    stop_idx  = lookup_codons_idx(seq, :stop, options)
-    puts "start codons idx: #{start_idx}"
-    puts "stop codons idx: #{stop_idx}"
-    res = get_longest(start_idx, stop_idx, [0, 1, 2], seq.size, options)
-    puts res
-    res.each do |frame|
-      frame.each do |range|
-        str = ''
-        str += "#{seq[range[:start]..range[:stop]]}" + '#'
-        str += '#:#' \
-          "size=#{seq[range[:start]..range[:stop]].size}"
-        puts str
-      end
-    end
-    res
-  end
-
-  def self.get_frame_idxs(idxs, frame, start = true)
+  # get indexes only from a given frame
+  # because of a bug the start flag must be given
+  #  indicating if it is looking for start or stop
+  #  codons in frame
+  def get_frame_idxs(idxs, frame, start = true)
     idxs.collect do |i|
       if start && (i - frame) % 3 == 0
         i
@@ -103,7 +208,10 @@ class ORF
     end.compact
   end
 
-  def self.decide_longest(start_idxs, stop_idxs, frame, seq_size, options)
+  #
+  # from the combination of start and stop indexes, find
+  #  the longest one
+  def decide_longest(start_idxs, stop_idxs, frame, seq_size)
     #
     seq_size -= (seq_size - frame) % 3
     start = start_idxs.clone
@@ -111,10 +219,12 @@ class ORF
     stop << seq_size - 1 if stop_idxs.empty?
     start << frame if start_idxs.empty?
     #
-    puts "frame: #{frame}"
-    puts "  start: #{start} | stop :#{stop}"
-    puts "  seq size: #{seq_size}"
-    puts "  #{options[:seq]}"
+    if options[:debug]
+      logger.info "frame: #{frame}"
+      logger.info "  start: #{start} | stop :#{stop}"
+      logger.info "  seq size: #{seq_size}"
+      logger.info "  #{seq[frame..seq_size]}"
+    end
     #
     valid = []
     fallback = []
@@ -127,26 +237,23 @@ class ORF
         # add a fall back where starts from begining
         fallback << { start: frame, stop: pos_stop, fallback: true } \
           if (pos_stop - frame) >= options[:min]
-        puts "  adding #{fallback.last} as fallback #{frame}..#{pos_stop}"
         # ignore if size of orf is smaller than minimum
-        next if (pos_stop - pos_start) < options[:min]
+        next if (pos_stop - pos_start + 1) < options[:min]
         # if all conditions hold add as valid orf
         if !start_idxs.empty? || !stop_idxs.empty?
-          valid << { start: pos_start, stop:  pos_stop }
+          valid << { start: pos_start, stop:  pos_stop, fallback: false }
         end
-        puts "  adding #{valid.last} as valid"
       end
       fallback << { start: pos_start, stop: seq_size - 1, fallback: true } \
         if (seq_size - 1 - pos_start) >= options[:min]
-      puts "  adding #{fallback.last} as fallback \#2 #{pos_start}..#{seq_size - 1}"
     end
     valid = fallback.uniq if valid.empty?
+    logger.info 'no ORF with start and stop codons,' \
+      ' defaulting to fallback' if valid.empty?
     valid
   end
 
-  def self.get_longest(start_idx, stop_idx,
-                       read_frame = [0, 1, 2], seq_size,
-                       options)
+  def get_longest(start_idx, stop_idx, seq_size, read_frame = [0, 1, 2])
     #
     start = [[], [], []]
     stop  = [[], [], []]
@@ -155,64 +262,9 @@ class ORF
       start[frame] = get_frame_idxs(start_idx, frame, true)
       stop[frame]  = get_frame_idxs(stop_idx, frame,  false)
       valid << decide_longest(start[frame], stop[frame],
-                              frame, seq_size, options)
+                              frame, seq_size)
     end
     #
     valid
-  end
-
-  #
-  def self.find(sequence, options)
-    return sequence if sequence.nil? || sequence.size == 0
-    # string should be more efficient
-    seq = sequence.to_s
-    # initialize best_start_idx to be at the end of
-    #  sequence
-    best_start_idx = sequence.size + 1
-    best_start_idx = find_idx(seq, best_start_idx, :start, '<', 1, options)
-    #
-    # necessary if start codon is not found
-    best_start_idx = 1 if best_start_idx == sequence.size + 1
-    # after start is found, trim the sequence to remove the
-    #  prefix and the start codon
-    seq = seq[(best_start_idx + 2)..(seq.size - 1)]
-    # best is before the first index
-    best_end_idx = 0
-    #
-    best_end_idx = find_idx(seq,
-                            best_end_idx,
-                            :stop,
-                            '>',
-                            seq.size,
-                            options) + best_start_idx + 3
-    #
-    if best_end_idx == 0
-      best_end_idx = sequence.size
-    else
-      best_end_idx += 1 unless best_end_idx + 2 > sequence.size
-    end
-    orf_seq = sequence.splice "#{best_start_idx}..#{best_end_idx}"
-    return '' if orf_seq.nil? || orf_seq.size < options[:min]
-    orf_seq
-  end
-
-  # find longest orf according to options
-  #  in config file
-  # sequence is a Bio::Sequence::NT class
-  def self.find_longest(sequence, options = DEFAULT_OPTIONS)
-    sequence = Bio::Sequence::NA.new(sequence) if sequence.class == String
-    #
-    result = {}
-    result[:nt] = find(sequence, options) if options[:direct]
-    if options[:reverse]
-      temp_orf = find(sequence.reverse, options)
-      result[:nt].size if temp_orf.size > result[:nt].size
-    end
-    result[:aa] = if result[:nt] == ''
-                    ''
-                  else
-                    result[:nt].translate
-                  end
-    result
   end
 end
