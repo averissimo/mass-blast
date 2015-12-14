9bff9cfbf62d1283e50d03230c8315d1a86af35f
André Veríssimo <afsverissimo@gmail.com>
2015-12-14 15:52:43 +0100

    adds find2

--- orf.rb
+++ orf.rb
@@ -5,11 +5,12 @@ require 'bio'
 class ORF
   #
   DEFAULT_OPTIONS = { start: %w(atg),
-                      stop:  %w(tag, taa, tga),
+                      stop:  %w(tag taa tga),
                       reverse: true,
                       direct: true,
-                      min: 0 }
+                      min: 6 }
   #
+
   def self.find_idx(seq, idx, option_name, cmp_sign, fallback_value, options)
     option_name = option_name.to_sym
     # if start option does not exist, then should
@@ -38,12 +39,134 @@ class ORF
     idx
   end
 
+  def self.lookup_codons_idx(seq, option_name, options)
+    idxs = []
+    option_name = option_name.to_sym
+    # if start option does not exist, then should
+    #  treat start of sequence as the start
+    if options[option_name] && !options[option_name].empty?
+      # iterate over all start codons to see which
+      #  is best
+      options[option_name].each do |codon|
+        temp_idxs = []
+        until (new_idx = \
+          seq.index(codon,
+                    (temp_idxs.empty? ? 0 : temp_idxs.last + 1))).nil?
+          temp_idxs << new_idx
+        end
+        idxs << temp_idxs
+      end
+    end
+    idxs.flatten.sort.collect do |idx|
+      if option_name == :start
+        idx + 3
+      elsif option_name == :stop
+        idx - 1
+      end
+    end
+  end
+
   #
-  def self.find(sequence, options)
-    return sequence if sequence.size == 0
+  def self.find2(sequence, options)
+    sequence = Bio::Sequence::NA.new(sequence) if sequence.class == String
+    return sequence if sequence.nil? || sequence.size == 0
     # string should be more efficient
     seq = sequence.to_s
-    # initi best_start_idx to be at the end of
+    #
+    options[:seq] = seq
+    #
+    start_idx = lookup_codons_idx(seq, :start, options)
+    stop_idx  = lookup_codons_idx(seq, :stop, options)
+    puts "start codons idx: #{start_idx}"
+    puts "stop codons idx: #{stop_idx}"
+    res = get_longest(start_idx, stop_idx, [0, 1, 2], seq.size, options)
+    puts res
+    res.each do |frame|
+      frame.each do |range|
+        str = ''
+        str += "#{seq[range[:start]..range[:stop]]}" + '#'
+        str += '#:#' \
+          "size=#{seq[range[:start]..range[:stop]].size}"
+        puts str
+      end
+    end
+    res
+  end
+
+  def self.get_frame_idxs(idxs, frame, start = true)
+    idxs.collect do |i|
+      if start && (i - frame) % 3 == 0
+        i
+      elsif !start && (i + 1 - frame) % 3 == 0
+        i
+      end
+    end.compact
+  end
+
+  def self.decide_longest(start_idxs, stop_idxs, frame, seq_size, options)
+    #
+    seq_size -= (seq_size - frame) % 3
+    start = start_idxs.clone
+    stop  = stop_idxs.clone
+    stop << seq_size - 1 if stop_idxs.empty?
+    start << frame if start_idxs.empty?
+    #
+    puts "frame: #{frame}"
+    puts "  start: #{start} | stop :#{stop}"
+    puts "  seq size: #{seq_size}"
+    puts "  #{options[:seq]}"
+    #
+    valid = []
+    fallback = []
+    # iterate on each start codon
+    start.each do |pos_start|
+      # iterate on each stop codon
+      stop.each do |pos_stop|
+        # ignore if start is bigger than stop index
+        next if pos_start >= pos_stop
+        # add a fall back where starts from begining
+        fallback << { start: frame, stop: pos_stop, fallback: true } \
+          if (pos_stop - frame) >= options[:min]
+        puts "  adding #{fallback.last} as fallback #{frame}..#{pos_stop}"
+        # ignore if size of orf is smaller than minimum
+        next if (pos_stop - pos_start) < options[:min]
+        # if all conditions hold add as valid orf
+        if !start_idxs.empty? || !stop_idxs.empty?
+          valid << { start: pos_start, stop:  pos_stop }
+        end
+        puts "  adding #{valid.last} as valid"
+      end
+      fallback << { start: pos_start, stop: seq_size - 1, fallback: true } \
+        if (seq_size - 1 - pos_start) >= options[:min]
+      puts "  adding #{fallback.last} as fallback \#2 #{pos_start}..#{seq_size - 1}"
+    end
+    valid = fallback.uniq if valid.empty?
+    valid
+  end
+
+  def self.get_longest(start_idx, stop_idx,
+                       read_frame = [0, 1, 2], seq_size,
+                       options)
+    #
+    start = [[], [], []]
+    stop  = [[], [], []]
+    valid = []
+    read_frame.each do |frame|
+      start[frame] = get_frame_idxs(start_idx, frame, true)
+      stop[frame]  = get_frame_idxs(stop_idx, frame,  false)
+      valid << decide_longest(start[frame], stop[frame],
+                              frame, seq_size, options)
+    end
+    #
+    valid
+  end
+
+  #
+  def self.find(sequence, options)
+    return sequence if sequence.nil? || sequence.size == 0
+    # string should be more efficient
+    seq = sequence.to_s
+    # initialize best_start_idx to be at the end of
     #  sequence
     best_start_idx = sequence.size + 1
     best_start_idx = find_idx(seq, best_start_idx, :start, '<', 1, options)
