b44891b8329a08d7fbd29f17fda13de23673118b
André Veríssimo <afsverissimo@gmail.com>
2015-12-28 12:21:23 +0000

    accept input config in script

--- script.rb
+++ script.rb
@@ -5,14 +5,18 @@ require 'configatron'
 #
 def run_user_config
   # configuration
-  config = YAML.load_file(File.expand_path('config/user.yml'))
+  if ARGV.empty?
+    config = YAML.load_file(File.expand_path('config/user.yml'))
+  else
+    config = YAML.load_file(File.expand_path(ARGV[0]))
+  end
   #
   b = nil
   case config['engine']
   when 'tblastn'
-    b = TBlastn.new
+    b = TBlastn.new(ARGV[0])
   when 'blastn'
-    b = Blastn.new
+    b = Blastn.new ARGV[0]
   else
     fail "Cannot recognize engine: #{config['engine']}. Please check" \
         ' documentation for implemented engines'
