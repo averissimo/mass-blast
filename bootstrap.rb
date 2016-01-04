a64d0cf070725ded24916e7402d3bea13decdcb8
André Veríssimo <afsverissimo@gmail.com>
2016-01-04 19:08:05 +0000

    moves bootstrap to rake task

--- bootstrap.rb
+++
@@ -1,3 +0,0 @@
-require_relative 'src/download'
-
-ExternalData.download
--- rakefile.rb
+++ rakefile.rb
@@ -8,7 +8,17 @@ RSpec::Core::RakeTask.new(:spec) do |t|
   # t.rspec_opts << ' more options'
 end
 
-Rake::FileList.new('output/*') do |fl|
+task default: :bootstrap
+
+desc 'Download all external databases'
+task :bootstrap do
+  require_relative 'src/download'
+  ExternalData.download
+end
+
+Rake::FileList.new('output/*',
+                   '**/db/*.nin', '**/db/*.nhr', '**/db/*.nsq',
+                   '**/db/*.btd', '**/db/*.bti') do |fl|
   CLOBBER << fl
   CLEAN << fl
 end
