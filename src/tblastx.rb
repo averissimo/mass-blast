2531e23599cded0d6ded37c7091119ea26d862b9
André Veríssimo <afsverissimo@gmail.com>
2016-01-08 15:35:07 +0000

    adds tblastx engine

---
+++ src/tblastx.rb
@@ -0,0 +1,21 @@
+require_relative 'blast'
+#
+#
+class TBlastx < Blast
+  #
+  # blastn blast
+  def blast(qfile, db, out_file, query_parent = nil)
+    query_parent = @store.query.parent if query_parent.nil?
+    #
+    qfile = File.join(query_parent, qfile) \
+      unless qfile.start_with?(query_parent)
+    # create command for this call
+    cmd = "tblastn -query \"#{qfile}\"" \
+          " -db \"#{db}\""                \
+          " #{@store.opts}"                                     \
+          " -out #{out_file}"                                   \
+          " -outfmt \"#{@store.format.outfmt}"                  \
+          " #{@store.format.specifiers.keys.join(' ')}\""
+    cmd
+  end
+end
