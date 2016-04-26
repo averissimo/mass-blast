eccc669a832a6a4f1318a26566bfed282c75b323
André Veríssimo <afsverissimo@gmail.com>
2016-04-26 17:22:43 +0100

    adds blastp engine

---
+++ src/blastp.rb
@@ -0,0 +1,21 @@
+require_relative 'blast'
+#
+#
+class Blastn < Blast
+  #
+  # blastn blast
+  def blast(qfile, db, out_file, query_parent = nil)
+    query_parent = @store.query.parent if query_parent.nil?
+    #
+    qfile = File.join(query_parent, qfile) \
+      unless qfile.start_with?(query_parent)
+    # create command for this call
+    cmd = "blastp -query \"#{qfile}\"" \
+          " -db \"#{db}\""                \
+          " #{@store.opts}"                                     \
+          " -out #{out_file}"                                   \
+          " -outfmt \"#{@store.format.outfmt}"                  \
+          " #{@store.format.specifiers.keys.join(' ')}\""
+    cmd
+  end
+end
