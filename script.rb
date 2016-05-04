0416e75b56846937508a2ebe476406f95cf892d6
André Veríssimo <afsverissimo@gmail.com>
2016-05-04 15:49:07 +0100

    adds some comments about benchmarking in script.rb

--- script.rb
+++ script.rb
@@ -1,3 +1,8 @@
 require_relative 'src/run.rb'
 #
 run_user_config((ARGV.empty? ? 'user.yml' : ARGV[0]))
+#
+# benchmarking
+#
+# for benchmarking a run 5 times do:
+# run_user_config((ARGV.empty? ? 'user.yml' : ARGV[0]), 5)
