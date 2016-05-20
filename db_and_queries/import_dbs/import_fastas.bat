5012fa4d6b963acdbda3cd53067d690b8570f313
André Veríssimo <afsverissimo@gmail.com>
2016-05-20 13:33:56 +0200

    corrects import fastas to take empty argument as nucl
    if using *.fasta and *.fas it will do the same file twice
      therefore, changed to only *.fas* just to make sure
      same file is not created twice

--- db_and_queries/import_dbs/import_fastas.bat
+++ db_and_queries/import_dbs/import_fastas.bat
@@ -1,22 +1,34 @@
 @echo off
 
-if [%1] == [] (
-%1 = nucl
+if "%1" == "" (
+SET arg=nucl
+
+echo "Creating databases with default argument of '%arg%', if files have protein sequence use:"
+echo "  import_fastas.bat prot"
+
+) else (
+SET arg=%1
+echo "Creating databases for %arg%."
 )
 
-if NOT "%1" == "nucl" if NOT "%1" == "prot" (
-echo "Error: ilegal value, expected 'nucl' or 'prot', not '%1%'"
-exit
+if NOT "%arg%" == "nucl" (
+	if NOT "%arg%" == "prot" (
+		echo "Error: ilegal value, expected 'nucl' or 'prot', not '%arg%'"
+    echo ""
+    echo "Usage: (by default nucl)"
+    echo "  import_fastas.bat [nucl|prot]"
+	)
+) ELSE (
+
+	for /r %%i in (*.fna *.fas*) do @makeblastdb -in %%i -dbtype "%arg%" -out "%%~ni" -title "%%~ni"
+
+	echo "moving all files to ../db"
+	move *.nhr ../db
+	move *.nin ../db
+	move *.nsq ../db
+	move *.nal ../db
+	move *.phr ../db
+	move *.pin ../db
+	move *.psq ../db
+	move *.pal ../db
 )
-
-for /r %%i in (*.fasta *.fna *.fas) do @makeblastdb -in %%i -dbtype "%1" -out "%%~ni" -title "%%~ni"
-
-echo "moving all files to ../db"
-move *.nhr ../db
-move *.nin ../db
-move *.nsq ../db
-move *.nal ../db
-move *.phr ../db
-move *.pin ../db
-move *.psq ../db
-move *.pal ../db
